175283272443e648b207ff65ad6dd2a317bc8b99
Alexandr <34417026+KamenkoAlexandr@users.noreply.github.com>
2018-02-25 18:31:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var  power = 0, intermediate = 0, result = 0, answer = number;
+    for (var i = 2; i <= base; i++) {
+        if (base % i == 0){
+            power = 0; result = 0; intermediate = number;
+            while (base % i == 0) {
+                power++;
+                base /= i;
+            }
+            while (intermediate / i > 0) {
+                intermediate = Math.trunc(intermediate / i);
+                result += intermediate;
+            }
+            answer = Math.min(answer, Math.floor(result / power));
+        }
+    }
+    return answer;
 }
